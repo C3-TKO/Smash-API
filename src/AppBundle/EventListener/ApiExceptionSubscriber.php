eb674b339452651ee9593bdeb4069ebb150e6e9e
Thomas Kolar <thomas.kolar@email.de>
2016-09-02 23:56:06 +0200

    Adds more documentation

--- src/AppBundle/EventListener/ApiExceptionSubscriber.php
+++ src/AppBundle/EventListener/ApiExceptionSubscriber.php
@@ -66,7 +66,10 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
 
         $data = $apiProblem->toArray();
 
-        // Provide an url for the error documentation
+        /**
+         * Provide an url for the error documentation
+         * @see: https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-03#section-3
+         */
         if ($data['type'] != 'about:blank') {
             $data['type'] = 'http://localhost:8000/docs/errors#'.$data['type'];
         }
