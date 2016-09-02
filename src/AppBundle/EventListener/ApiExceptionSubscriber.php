e985851c3e8920b6d9b348b462ecc2d190a4ef8b
Thomas Kolar <thomas.kolar@email.de>
2016-09-02 23:52:54 +0200

    Introduces a different exception handling in debug mode

--- src/AppBundle/EventListener/ApiExceptionSubscriber.php
+++ src/AppBundle/EventListener/ApiExceptionSubscriber.php
@@ -20,30 +20,59 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
+    /**
+     * A flag to indicate if debugging mode is enabled
+     *
+     * @var bool
+     */
+    private $debug = false;
+
+    public function __construct($debug)
+    {
+        $this->debug = $debug;
+    }
+
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
+
+        $statusCode = $exception instanceof HttpExceptionInterface
+            ? $exception->getStatusCode()
+            : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        // Allow HTTP 500 on ognoing exception to be handled by symfony when running in debug mode
+        if ($this->debug && $statusCode >= Response::HTTP_INTERNAL_SERVER_ERROR) {
+            return;
+        }
+
         if ($exception instanceof ApiProblemException) {
             $apiProblem = $exception->getApiProblem();
         } else {
-            $statusCode = $exception instanceof HttpExceptionInterface
-                ? $exception->getStatusCode()
-                : Response::HTTP_INTERNAL_SERVER_ERROR;
-
             $apiProblem = new ApiProblem(
                 $statusCode
             );
 
             /**
              * @see https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-03#section-3.1
+             *
+             * If it is an HttpException message (e.g. for 404, 403), we'll say as a rule that the exception message is
+             * safe for the client. Otherwise, it could be some sensitive low-level exception, which should *not* be
+             * exposed
              */
             if ($exception instanceof HttpExceptionInterface) {
                 $apiProblem->set('detail', $exception->getMessage());
             }
         }
 
+        $data = $apiProblem->toArray();
+
+        // Provide an url for the error documentation
+        if ($data['type'] != 'about:blank') {
+            $data['type'] = 'http://localhost:8000/docs/errors#'.$data['type'];
+        }
+
         $response = new JsonResponse(
-            $apiProblem->toArray(),
+            $data,
             $apiProblem->getStatusCode()
         );
         $response->headers->set('Content-Type', 'application/json+problem');
