9e13d2bc4c02d988dab44958ec502f653797495c
Thomas Kolar <thomas.kolar@email.de>
2016-09-18 22:02:50 +0200

    Removes unnecessary use statements and adds a security annotation

--- src/AppBundle/Controller/Api/PlayerController.php
+++ src/AppBundle/Controller/Api/PlayerController.php
@@ -10,10 +10,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Form;
 use AppBundle\Entity\Player;
-use Pagerfanta\Adapter\DoctrineORMAdapter;
-use Pagerfanta\Pagerfanta;
-use AppBundle\Pagination\PaginatedCollection;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+/**
+ * @Security("is_granted('ROLE_USER')")
+ */
 class PlayerController extends BaseController
 {
     /**
