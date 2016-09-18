1fee0115ac578f5db810a083fddfb68e7a5c6883
Thomas Kolar <thomas.kolar@email.de>
2016-09-18 13:54:29 +0200

    Implements the create token action

--- src/AppBundle/Controller/Api/TokenController.php
+++ src/AppBundle/Controller/Api/TokenController.php
@@ -8,7 +8,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class TokenController extends BaseController
 {
@@ -18,6 +19,25 @@ class TokenController extends BaseController
      */
     public function createAction(Request $request)
     {
-        return new Response('TOKEN!');
+        $user = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findOneBy(['username' => $request->getUser()]);
+
+        if (!$user) {
+            throw $this->createNotFoundException();
+        }
+
+        $isValid = $this->get('security.password_encoder')
+            ->isPasswordValid($user, $request->getPassword());
+        if (!$isValid) {
+            throw new BadCredentialsException();
+        }
+
+        $token = $this->get('lexik_jwt_authentication.encoder')
+            ->encode([
+                'username' => $user->getUsername()
+            ]);
+
+        return new JsonResponse(['token' => $token]);
     }
 }
