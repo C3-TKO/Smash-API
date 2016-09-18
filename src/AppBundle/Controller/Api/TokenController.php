8f451f314bc8c086f8720433a1465cf368819de6
Thomas Kolar <thomas.kolar@email.de>
2016-09-18 22:10:07 +0200

    Adds a dummy route for better debugging of unit tests

--- src/AppBundle/Controller/Api/TokenController.php
+++ src/AppBundle/Controller/Api/TokenController.php
@@ -40,4 +40,14 @@ class TokenController extends BaseController
 
         return new JsonResponse(['token' => $token]);
     }
+
+    /**
+     * @return Response
+     *
+     * @Route("/dummy", name="security_login_check")
+     * @Route("/dummy", name="security_login_form")
+     */
+    public function dummy() {
+        return new Response('DOH');
+    }
 }
