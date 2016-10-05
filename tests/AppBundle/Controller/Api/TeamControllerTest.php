ebdea3234d82b2915fc3c8aeb71dfa0330cea74a
Thomas Kolar <thomas.kolar@email.de>
2016-10-06 00:25:06 +0200

    Adds the unit test for the post team route

--- tests/AppBundle/Controller/Api/TeamControllerTest.php
+++ tests/AppBundle/Controller/Api/TeamControllerTest.php
@@ -34,4 +34,41 @@ class TeamControllerTest extends ApiTestCase
         $this->asserter()->assertResponsePropertyIsArray($response, 'items');
         $this->asserter()->assertResponsePropertyCount($response, 'items', 1);
     }
+
+    /**
+     * @test
+     */
+    public function postValidTeamShouldCreateATeam()
+    {
+        $this->createPlayers(['ACME', 'INC.']);
+
+        $data = array(
+            'id_player_a' => 1, // ACME
+            'id_player_b' => 2  // INC
+        );
+
+        $response = $this->client->post('/api/teams', [
+            'body' => json_encode($data),
+            'headers' => $this->getAuthorizedHeaders(self::USERNAME_TEST_USER)
+        ]);
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->getHeader('Content-Type')[0]);
+        $this->assertStringEndsWith('/api/teams/1', $response->getHeader('Location')[0]);
+        $this->asserter()->assertResponsePropertiesExist($response, array(
+            'id',
+            'id_player_a',
+            'id_player_b',
+            'name'
+        ));
+        $this->asserter()->assertResponsePropertyEquals($response, 'id', 1);
+        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_a', 1);
+        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_b', 2);
+        $this->asserter()->assertResponsePropertyEquals($response, 'name', '');
+
+        // Only one player should be in database
+        $em = $this->getEntityManager();
+        $teams = $em->getRepository('AppBundle:Team')->findAll();
+        $this->assertEquals(1, count($teams));
+    }
 }
