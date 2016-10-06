89b0d5e14dbfb7b297d8d39fc9758deee837b8f0
Thomas Kolar <thomas.kolar@email.de>
2016-10-06 18:22:30 +0200

    Adds the form type for team

---
+++ src/AppBundle/Form/TeamType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use AppBundle\Form\PlayerType;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('id', IntegerType::class, ['disabled' => $options['is_edit']])
+            ->add('id_player_a', new PlayerType())
+            ->add('id_player_b', new PlayerType())
+            ->add('name', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Team',
+            'is_edit' => false,
+            // Setting the csrf protection globally to false as this api will not store the user within session cookies
+            // Keep in mind that the protection should be turned on again if his form type will be used within an ui
+            // form as well!
+            'csrf_protection' => false
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'team';
+    }
+}
