ec967e70c58368f3e830fb346a8665a61d02f50f
Thomas Kolar <thomas.kolar@email.de>
2016-10-15 01:03:56 +0200

    Adds assertions for test scenarios with access to non existing entities

--- tests/AppBundle/Controller/Api/TeamControllerTest.php
+++ tests/AppBundle/Controller/Api/TeamControllerTest.php
@@ -64,7 +64,24 @@ class TeamControllerTest extends ApiTestCase
     /**
      * @test
      */
-    public function putNameShouldUpdateTeam()
+    public function getNotExistingTeamShouldThrow404()
+    {
+        $data = ['name' => 'TestTeamNameUpdated'];
+
+        $response = $this->client->get('/api/teams/1', [
+            'body' => json_encode($data),
+            'headers' => $this->getAuthorizedHeaders(self::USERNAME_TEST_USER)
+        ]);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertEquals('application/problem+json', $response->getHeader('Content-Type')[0]);
+        $this->assertAccessToNotExistingEntity($response, 'team', 1);
+    }
+
+    /**
+     * @test
+     */
+    public function putNameShouldUpdateTeamName()
     {
         $this->createPlayers(['ACME', 'INC.']);
 
@@ -83,14 +100,30 @@ class TeamControllerTest extends ApiTestCase
 
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
         $this->assertEquals('application/hal+json', $response->getHeader('Content-Type')[0]);
-        $this->assertStringEndsWith('/api/players/1', $response->getHeader('Location')[0]);
         $this->assertTeamModelSpecifictaion($response);
 
         $this->asserter()->assertResponsePropertyEquals($response, 'id', 1);
-        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_a', 2);
-        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_b', 3);
+        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_a', 1);
+        $this->asserter()->assertResponsePropertyEquals($response, 'id_player_b', 2);
         $this->asserter()->assertResponsePropertyEquals($response, 'name', 'TestTeamNameUpdated');
         $this->asserter()->assertResponsePropertyEquals($response, '_links.self.href', $this->adjustUri('/api/teams/1'));
+    }
+
+    /**
+     * @test
+     */
+    public function putNameOnNotExistingTeamShouldThrow404()
+    {
+        $data = ['name' => 'TestTeamNameUpdated'];
+
+        $response = $this->client->put('/api/teams/1/name', [
+            'body' => json_encode($data),
+            'headers' => $this->getAuthorizedHeaders(self::USERNAME_TEST_USER)
+        ]);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertEquals('application/problem+json', $response->getHeader('Content-Type')[0]);
+        $this->assertAccessToNotExistingEntity($response, 'team', 1);
     }
 
     private function assertTeamModelSpecifictaion(ResponseInterface $response) {
@@ -102,5 +135,20 @@ class TeamControllerTest extends ApiTestCase
             '_links.player_a.href',
             '_links.player_b.href'
         ));
+    }
+
+    private function assertAccessToNotExistingEntity(ResponseInterface $response, $entityName, $id)
+    {
+        $this->asserter()->assertResponsePropertiesExist($response, array(
+            'detail',
+            'status',
+            'type',
+            'title'
+        ));
+
+        $this->asserter()->assertResponsePropertyEquals($response, 'detail', sprintf('No %s found with id %u', $entityName, $id));
+        $this->asserter()->assertResponsePropertyEquals($response, 'status', '404');
+        $this->asserter()->assertResponsePropertyEquals($response, 'type', 'about:blank');
+        $this->asserter()->assertResponsePropertyEquals($response, 'title', 'Not Found');
     }
 }
