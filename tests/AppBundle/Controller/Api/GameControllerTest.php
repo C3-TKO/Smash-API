dde1ea855113e7dd23d1cf5688aa61723f8b0ec2
Thomas Kolar <thomas.kolar@email.de>
2016-10-24 00:12:58 +0200

    Add the unit test for post->api/games

--- tests/AppBundle/Controller/Api/GameControllerTest.php
+++ tests/AppBundle/Controller/Api/GameControllerTest.php
@@ -56,7 +56,7 @@ class GameControllerTest extends ApiTestCase
         $this->asserter()->assertResponsePropertyEquals($response, 'id', 1);
         $this->asserter()->assertResponsePropertyEquals($response, 'id_round', 1);
         $this->asserter()->assertResponsePropertyEquals($response, 'id_team_a', 1);
-        $this->asserter()->assertResponsePropertyEquals($response, 'id_team_b', 1);
+        $this->asserter()->assertResponsePropertyEquals($response, 'id_team_b', 2);
         $this->asserter()->assertResponsePropertyEquals($response, 'team_a_score', 21);
         $this->asserter()->assertResponsePropertyEquals($response, 'team_b_score', 19);
 
